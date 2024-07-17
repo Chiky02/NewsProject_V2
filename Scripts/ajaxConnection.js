22264fef94273eb2aa3f14f260ce41a9787266cc
Chiky02 <juanballen.02@gmail.com>
2024-07-16 22:06:47 -0500

    Empezando a mejorar el login con medidas de seguridad y asegurando el uso del patron singleton

---
+++ Scripts/ajaxConnection.js
@@ -0,0 +1,44 @@
+  // Import the functions you need from the SDKs you need
+  function ajax(data,action) {
+
+    alert(data);
+    $.ajax({
+      // la URL para la petición
+      url: 'Php/'+action+'.php',
+      // la información a enviar
+      data: JSON.stringify(data),
+      // especifica si será una petición POST o GET
+      type: 'POST',
+  
+      // el tipo de información que se espera de respuesta
+      dataType: 'text',
+      cache: false,
+      // código a ejecutar si la petición es satisfactoria;
+  
+      beforeSend: function () {
+        console.log("cargando... no se desespere");
+      },
+      success: function (json) {
+  
+  
+  
+        if (json.length == 0) {
+          alert("No se ha recibido ninguna respuesta");
+        }
+  
+        else {
+          alert(json);
+        }
+      },
+      // código a ejecutar si la petición falla;
+      error: function (xhr, status) {
+        alert("Ha ocurrido un error inesperado");
+        alert(status);
+      },
+      /*
+              // código a ejecutar sin importar si la petición falló o no
+              complete : function(xhr, status) {
+                  alert('Petición realizada');
+              }*/
+    });
+  }
