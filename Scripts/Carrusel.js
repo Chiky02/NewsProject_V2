a58d8e3dc9afa80d8188e0a3b45acba7b9b3caab
Chiky02 <juanballen.02@gmail.com>
2024-07-23 16:45:44 -0500

    Se ha agregado en el encabezado de contactos informacion relevante

--- Scripts/Carrusel.js
+++ Scripts/Carrusel.js
@@ -13,11 +13,11 @@ let imagenes = [
         "url": "https://i.blogs.es/0ca9a6/aa/1366_2000.jpeg",
         "nombre": "Proyecto 03",
         "descripcion": "El carrusel fué añadido"
-    },  {
+    }, {
         "url": "https://e0.pxfuel.com/wallpapers/976/989/desktop-wallpaper-1280-x-720.jpg",
         "nombre": "El carrusel fué añadido El carrusel fué añadido El carrusel fué añadido El carrusel fué añadido ",
         "descripcion": "El carrusel fué añadido"
-    },  {
+    }, {
         "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGcKDNporYglmWAEtJ-7SXvopoO2b7-IOthw&s",
         "nombre": "Proyecto 03",
         "descripcion": "El carrusel fué añadido"
@@ -35,10 +35,10 @@ let actual = 0;
 
 posicionCarrusel()
 
-atras.addEventListener('click', function(){
-    actual -=1
+atras.addEventListener('click', function () {
+    actual -= 1
 
-    if (actual == -1){
+    if (actual == -1) {
         actual = imagenes.length - 1
     }
 
@@ -46,9 +46,9 @@ atras.addEventListener('click', function(){
     texto.innerHTML = `
     <p>${imagenes[actual].nombre}</p>`
     //<p>${imagenes[actual].descripcion}</p>
-    
+
     posicionCarrusel();
-});  
+});
 
 
 const cambiarNoticiaAuto = () => {
@@ -66,10 +66,10 @@ setInterval(siguienteNoticia, 10000);
 adelante.addEventListener('click', siguienteNoticia);
 
 
-function siguienteNoticia(){
-    actual +=1;
+function siguienteNoticia() {
+    actual += 1;
 
-    if (actual == imagenes.length){
+    if (actual == imagenes.length) {
         actual = 0;
     }
 
@@ -82,12 +82,12 @@ function siguienteNoticia(){
 
 function posicionCarrusel() {
     puntos.innerHTML = ""
-    for (var i = 0; i <imagenes.length; i++){
-        if(i == actual){
+    for (var i = 0; i < imagenes.length; i++) {
+        if (i == actual) {
             puntos.innerHTML += '<p class="bold">.<p>'
         }
-        else{
+        else {
             puntos.innerHTML += '<p>.<p>'
         }
-    } 
+    }
 }
