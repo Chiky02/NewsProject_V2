3922f5926abbc3478dad736833063a74121a20a6
Chiky02 <juanballen.02@gmail.com>
2024-07-30 00:03:13 -0500

    Se mejoró el panel de administrador, se agregó la pagina de aceptar o denegar noticias junto con su conexion backend, hace falta darle funcionalidad a los botones

--- indexAdmin.php
+++ indexAdmin.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,26 +8,30 @@
     <link rel="stylesheet" href="Style/style.css">
     <link rel="stylesheet" href="Style/admin.css">
 </head>
+
 <body>
     <div class="admin-container">
         <div class="profile">
             <img src="Images/Logo2.png" alt="Foto del Administrador">
             <div>
-                <h2>Nombre del Administrador</h2>
-                <p>Correo: admin@ejemplo.com</p>
+                <h2><?php session_start(); // Iniciar la sesión
+                    echo $_SESSION['user_name']   ?> </h2>
+                <p><?php
+                    echo $_SESSION['user_email'] ?></p>
             </div>
         </div>
 
         <div class="admin-options">
             <a href="index.html">&#128064;Ver Sitio</a>
-            <a href="index.html">manejar usuarios</a>
+            <a href="#">manejar usuarios</a>
             <a href="CreateNew-V3.html">Crear Noticia</a>
-            <a href="aceptar/devolver-noticia.html">Aceptar Noticia</a>
-            
+            <a href="aceptNew.html">Aceptar Noticia</a>
+
             <a href="borrar-noticia.html">Borrar Noticia</a>
-            <a href="agregar-info-slider.html">Agregar Info al Slider</a>
+            <a href="CreateNew.html">Agregar Info al Slider</a>
             <a href="eliminar-info-slider.html">Eliminar Info del Slider</a>
         </div>
     </div>
 </body>
+
 </html>
